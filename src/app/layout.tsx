2f26b423f3ff42a25a7fc23259a318f866278eee
Filipe Pereira <f29.pereira@gmail.com>
2025-11-07 10:27:46 +0000

    Next.js installation, Add font Inconsolata

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inconsolata } from "next/font/google";
+import "./globals.css";
+
+const inconsolata = Inconsolata({
+  variable: "--font-inconsolata",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "GitHub User Lists",
+  description: "GitHub User Lists",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inconsolata.variable}`}>{children}</body>
+    </html>
+  );
+}
