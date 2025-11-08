569181dbf9b3876090a519a3d103e8bd05123508
Filipe Pereira <f29.pereira@gmail.com>
2025-11-08 10:21:41 +0000

    Update globals.css styling and Add Users component

---
+++ src/app/components/Users/Users.tsx
@@ -0,0 +1,46 @@
+import styles from "./Users.module.css";
+import { UserTableProps } from "@/app/types";
+import { FaGithub } from "../../icons/icons";
+
+/**
+ * Renders users list with:
+ * - Header
+ * - Users info with: Avatar image, Name and GitHub profile Link
+ *
+ * Props are defined in {@link UserTableProps}.
+ */
+export default function Users({ userList }: UserTableProps) {
+  return (
+    <div className={`flex-col-center`}>
+      {/*Header*/}
+      <div className={`${styles.container} ${styles.header}`}>
+        <span>Avatar</span>
+        <span>Name</span>
+        <span>Profile</span>
+      </div>
+
+      {/*Users*/}
+      {userList?.map((user) => (
+        <div key={user.id} className={`${styles.container} ${styles.user}`}>
+          <div>
+            <img
+              className={styles.avatar}
+              src={user.avatar}
+              alt="User avatar"
+            />
+          </div>
+          <div>{user.name}</div>
+          <div>
+            <a
+              href={user.url}
+              target="_blank"
+              className={`flex-center ${styles.profileCont}`}
+            >
+              <FaGithub className={styles.gitHubIcon} />
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
